52a88387870530ad2a2f957c25576147ccf93e63
rjsanjaymandal <rjsanjaymandal@gmail.com>
2022-09-09 15:06:05 +0530

    ass 15

--- python/solutions/ass 15/09.py
+++ python/solutions/ass 15/09.py
@@ -1,6 +1,6 @@
 '''9. Write a python script to check if a string contains
  only characters of the alphabet.'''
 
- string= input("enter the string ==>> " )
-print(string.isalpha())
-print(string.isdigit())
+string= input("enter the string ==>> " )
+print("alphabets only ==>> ",string.isalpha())
+print("alphnumeric ==>> ",string.isalnum())
---
+++ python/solutions/ass 15/10.py
@@ -0,0 +1,5 @@
+'''10. Write a python script to convert an integer to a string.'''
+
+n=int(int(input("enter the integer ==>> ")))
+s=str(n)
+print(s,type(s))
